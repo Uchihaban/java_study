7a6223056d691bae3dd440c545609432f78241f0
zhengban <1228340359@qq.com>
2018-03-23 22:01:22 +0800

    JAVA

---
+++ src/day6/Solution.java
@@ -0,0 +1,33 @@
+package day6;
+
+/*把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+输入一个非递减排序的数组的一个旋转，输出旋转数组的最小元素。 
+例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1。 
+NOTE：给出的所有元素都大于0，若数组大小为0，请返回0。*/
+
+public class Solution {
+    public int minNumberInRotateArray(int [] array) {
+        int[] brr = new int[array.length];
+        int index = 0;
+        int i =0;
+        for(;i<array.length;i++){
+            if(array[i]>array[i+1]){
+                break;
+            }
+        }
+        for(int j=i+1;j<array.length;j++){
+            brr[index++]=array[j];
+        }
+        for(int j =0;j<array.length-index;j++){
+            brr[index+1]=array[j];
+        }
+        return brr[0];
+        
+        
+    }
+    public static void main(String[] args){
+    	Solution s = new Solution();
+        int[] arr = {3,4,5,1,2};
+    	s.minNumberInRotateArray(arr);
+    }
+}
